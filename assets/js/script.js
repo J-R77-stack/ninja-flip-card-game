b3a99265b03ebc27fe577b587f1b58c5960a67bf
J-R77-stack <johnnyramsay81@gmail.com>
2021-09-02 18:22:22 +0000

    create function of newDeck to create deck of cards with every combination

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const SUITS = ["♥", "♦", "♠", "♣"]
 const VALUES = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
 
 class Deck {
-    constructor(cards) {
+    constructor(cards = newDeck()) {
         this.cards = cards
     }
 }
@@ -13,3 +13,13 @@ class Card {
         this.value = value
     }
 }
+
+function newDeck() {
+    return SUITS.flatMap(suit => {
+        return VALUES.map(value => {
+            return new Card(suit, value)
+        })
+    })
+}
+
+
